8a7455adf6d8c5ab1ccd3e1d02d42becc1f8f9b2
yourims <yourims@2508997c-744b-0410-8238-c72e70b22124>
2008-05-08 11:29:25 +0000

    no comment

--- src/gestrh/model/Employes.java
+++ src/gestrh/model/Employes.java
@@ -1,5 +1,36 @@
 package gestrh.model;
 
-public class Employes {
+import java.io.Serializable;
+import java.util.List;
 
+import fr.epsi.localisation.domain.model.Departement;
+
+
+@Entity
+@Table(name = "EMPLOYES")
+public class Employes implements Serializable{
+
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue
+    @Column(name="id_emp")
+	public int id_emp;
+	
+	@Column(name="prenom_emp")
+	public String prenom_emp;
+	
+	@OneToMany(targetEntity=fr.epsi.localisation.domain.model.Departement.class, cascade=CascadeType.ALL, mappedBy="pays", fetch = FetchType.EAGER )
+	private List<Departement> departements;
+	public List<Departement> getDepartements()
+	{
+		return departements;
+	}
+
+
+	public Pays(String nom) {
+		this.nom = nom;
+	}
+	public Pays(){}
 }
